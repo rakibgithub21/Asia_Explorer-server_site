53129111b99ff26acd6610bb0ca646a755ea52ad
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-04-27 07:03:04 +0600

    basic setup

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware:
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('My server is running')
+})
+
+app.listen(port, () => {
+    console.log(`my server is running on ${port}`);
+})
