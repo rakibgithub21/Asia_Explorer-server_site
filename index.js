90e78e1c2573a5c57eb42763df71f07f1b8b9ea6
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-04-27 21:44:26 +0600

    make another api for country

--- index.js
+++ index.js
@@ -38,6 +38,14 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/country/:countryName', async (req, res) => {
+            const countryName = req.params.countryName;
+            const query = { country: countryName }
+            const cursor = touristCollection.find(query)
+            const result = await cursor.toArray(cursor)
+            res.send(result)
+        })
+
         app.get('/tourist-spot/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
